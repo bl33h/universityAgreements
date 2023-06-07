e52adcf9596eb8683c67e42e546fd4325d4c6963
bl33h <per21371@uvg.edu.gt>
2023-06-06 21:33:36 -0600

    supabase settings placed.

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-    "https://cchcbbjgpcwuugrjedsl.supabase.co/", 
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjaGNiYmpncGN3dXVncmplZHNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMzMjk2NDEsImV4cCI6MTk5ODkwNTY0MX0.AehLrTlHHIt04_0Xw4B6iEO3EKXpG9H5EG2NiKCZtAw",
+    "https://abfeeptgrejiyytvtslx.supabase.co", 
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiZmVlcHRncmVqaXl5dHZ0c2x4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYxMDczMTUsImV4cCI6MjAwMTY4MzMxNX0.mPlOEyQzmsHnbyDnu5MHUn53WkqHLIyDJQk6eANOhm8",
 )
